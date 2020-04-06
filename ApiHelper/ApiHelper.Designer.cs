00a176d073abc36d1ab3581e50bfa2623826945f
joe.wyhb@outlook.com <joe.wyhb@outlook.com>
2020-04-06 23:14:12 +0800

    init

---
+++ ApiHelper/ApiHelper.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace ApiHelper
+{
+    partial class ApiHelper
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ApiHelper));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openJsonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ヘルプToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.versionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtEndPoint = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMethod = new System.Windows.Forms.TextBox();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.txtHeader = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btn = new System.Windows.Forms.Button();
+            this.tab = new System.Windows.Forms.TabControl();
+            this.tab1 = new System.Windows.Forms.TabPage();
+            this.tab2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.tab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.ヘルプToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(3, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(803, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openJsonToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(53, 22);
+            this.fileToolStripMenuItem.Text = "ファイル";
+            // 
+            // openJsonToolStripMenuItem
+            // 
+            this.openJsonToolStripMenuItem.Name = "openJsonToolStripMenuItem";
+            this.openJsonToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openJsonToolStripMenuItem.Text = "Jsonファイルを開く";
+            this.openJsonToolStripMenuItem.Click += new System.EventHandler(this.openJsonToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "閉じる";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // ヘルプToolStripMenuItem
+            // 
+            this.ヘルプToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewHelpToolStripMenuItem,
+            this.versionToolStripMenuItem});
+            this.ヘルプToolStripMenuItem.Name = "ヘルプToolStripMenuItem";
+            this.ヘルプToolStripMenuItem.Size = new System.Drawing.Size(48, 22);
+            this.ヘルプToolStripMenuItem.Text = "ヘルプ";
+            // 
+            // viewHelpToolStripMenuItem
+            // 
+            this.viewHelpToolStripMenuItem.Name = "viewHelpToolStripMenuItem";
+            this.viewHelpToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.viewHelpToolStripMenuItem.Text = "ヘルプの表示";
+            this.viewHelpToolStripMenuItem.Click += new System.EventHandler(this.viewHelpToolStripMenuItem_Click);
+            // 
+            // versionToolStripMenuItem
+            // 
+            this.versionToolStripMenuItem.Name = "versionToolStripMenuItem";
+            this.versionToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.versionToolStripMenuItem.Text = "バージョン";
+            this.versionToolStripMenuItem.Click += new System.EventHandler(this.versionToolStripMenuItem_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl1.Location = new System.Drawing.Point(10, 36);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(69, 15);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "エンドポイント";
+            // 
+            // txtEndPoint
+            // 
+            this.txtEndPoint.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtEndPoint.Location = new System.Drawing.Point(87, 32);
+            this.txtEndPoint.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.txtEndPoint.Name = "txtEndPoint";
+            this.txtEndPoint.ReadOnly = true;
+            this.txtEndPoint.Size = new System.Drawing.Size(703, 23);
+            this.txtEndPoint.TabIndex = 2;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl2.Location = new System.Drawing.Point(10, 59);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(26, 15);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "パス";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtPath.Location = new System.Drawing.Point(87, 54);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(703, 23);
+            this.txtPath.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(10, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "メソッド";
+            // 
+            // txtMethod
+            // 
+            this.txtMethod.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtMethod.Location = new System.Drawing.Point(87, 76);
+            this.txtMethod.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.txtMethod.Name = "txtMethod";
+            this.txtMethod.ReadOnly = true;
+            this.txtMethod.Size = new System.Drawing.Size(703, 23);
+            this.txtMethod.TabIndex = 6;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl4.Location = new System.Drawing.Point(11, 103);
+            this.lbl4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(33, 15);
+            this.lbl4.TabIndex = 7;
+            this.lbl4.Text = "ヘッダ";
+            // 
+            // txtHeader
+            // 
+            this.txtHeader.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtHeader.Location = new System.Drawing.Point(87, 98);
+            this.txtHeader.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.txtHeader.Name = "txtHeader";
+            this.txtHeader.ReadOnly = true;
+            this.txtHeader.Size = new System.Drawing.Size(703, 23);
+            this.txtHeader.TabIndex = 8;
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv.Location = new System.Drawing.Point(12, 146);
+            this.dgv.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.RowHeadersWidth = 82;
+            this.dgv.RowTemplate.Height = 33;
+            this.dgv.Size = new System.Drawing.Size(346, 372);
+            this.dgv.TabIndex = 9;
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn.Location = new System.Drawing.Point(360, 309);
+            this.btn.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(43, 27);
+            this.btn.TabIndex = 10;
+            this.btn.Text = "実行";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // tab
+            // 
+            this.tab.Controls.Add(this.tab1);
+            this.tab.Controls.Add(this.tab2);
+            this.tab.Location = new System.Drawing.Point(407, 124);
+            this.tab.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.tab.Name = "tab";
+            this.tab.SelectedIndex = 0;
+            this.tab.Size = new System.Drawing.Size(377, 394);
+            this.tab.TabIndex = 11;
+            // 
+            // tab1
+            // 
+            this.tab1.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tab1.Location = new System.Drawing.Point(4, 22);
+            this.tab1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.tab1.Name = "tab1";
+            this.tab1.Padding = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.tab1.Size = new System.Drawing.Size(369, 368);
+            this.tab1.TabIndex = 0;
+            this.tab1.Text = "tab1";
+            this.tab1.UseVisualStyleBackColor = true;
+            // 
+            // tab2
+            // 
+            this.tab2.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tab2.Location = new System.Drawing.Point(4, 22);
+            this.tab2.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.tab2.Name = "tab2";
+            this.tab2.Padding = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.tab2.Size = new System.Drawing.Size(369, 368);
+            this.tab2.TabIndex = 1;
+            this.tab2.Text = "tab2";
+            this.tab2.UseVisualStyleBackColor = true;
+            // 
+            // ApiHelper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 554);
+            this.Controls.Add(this.tab);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.txtHeader);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.txtMethod);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.txtEndPoint);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.Name = "ApiHelper";
+            this.Text = "ApiHelper";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.tab.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openJsonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ヘルプToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewHelpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem versionToolStripMenuItem;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtEndPoint;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMethod;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.TextBox txtHeader;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TabControl tab;
+        private System.Windows.Forms.TabPage tab1;
+        private System.Windows.Forms.TabPage tab2;
+    }
+}
+
---
+++ ApiHelper/ApiHelper.cs
@@ -0,0 +1,198 @@
+﻿using Codeplex.Data;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApiHelper
+{
+    public partial class ApiHelper : Form
+    {
+        public ApiHelper()
+        {
+            InitializeComponent();
+        }
+
+        private CheckBox headerCheckBox = new CheckBox();
+
+        private void openJsonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.FileName = "sample.json";
+            ofd.InitialDirectory = @"E:\work";
+            ofd.Filter = "Jsonファイル(*.json)|";
+            ofd.FilterIndex = 2;
+            ofd.Title = "開くファイルを選択してください";
+            ofd.RestoreDirectory = true;
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                var stm = ofd.OpenFile();
+                if (stm != null)
+                {
+                    var api = DynamicJson.Parse(stm);
+                    this.txtEndPoint.Text = api.EndPoint;
+                    this.txtPath.Text = api.Path;
+                    this.txtMethod.Text = api.Method;
+                    this.txtHeader.Text = api.Header;
+                    headerCheckBox.Location = new Point(6, 4);
+                    headerCheckBox.BackColor = Color.White;
+                    headerCheckBox.Size = new Size(14, 14);
+
+                    headerCheckBox.Click += new EventHandler(headerCheckBox_Clicked);
+                    dgv.Controls.Add(headerCheckBox);
+
+                    DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn
+                    {
+                        HeaderText = "",
+                        Width = 30,
+                        Name = "checkBoxColumn"
+                    };
+                    this.dgv.Columns.Add(checkBoxColumn);
+                    this.dgv.Columns.Add("Case", "ケース");
+                    this.dgv.Columns.Add("Request", "リクエストデータ");
+                    foreach (var rc in api.RequestCase)
+                    {
+                        this.dgv.Rows.Add(false, rc.Case, rc.Request);
+                    }
+                    foreach (DataGridViewColumn dgvc in this.dgv.Columns)
+                    {
+                        dgvc.SortMode = DataGridViewColumnSortMode.NotSortable;
+                    }
+                    this.dgv.Columns[0].Width = 20;
+                    this.dgv.Columns[1].Width = 60;
+                    var headersCellStyle = new DataGridViewCellStyle
+                    {
+                        Alignment = DataGridViewContentAlignment.MiddleCenter,
+                        BackColor = Color.Ivory,
+                        ForeColor = Color.IndianRed,
+                        Font = new Font("Meiryo UI", 9, FontStyle.Bold)
+                    };
+                    this.dgv.ColumnHeadersDefaultCellStyle = headersCellStyle;
+                    this.dgv.CellContentClick += new DataGridViewCellEventHandler(DataGridView_CellClick);
+                }
+                else
+                {
+                    MessageBox.Show("Jsonファイルにはデータがありません！", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void headerCheckBox_Clicked(object sender, EventArgs e)
+        {
+            this.dgv.EndEdit();
+
+            foreach (DataGridViewRow row in this.dgv.Rows)
+            {
+                DataGridViewCheckBoxCell checkBox = (row.Cells["checkBoxColumn"] as DataGridViewCheckBoxCell);
+                checkBox.Value = headerCheckBox.Checked;
+            }
+        }
+
+        private void DataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex == 0)
+            {
+                bool isChecked = true;
+                foreach (DataGridViewRow row in this.dgv.Rows)
+                {
+                    if (Convert.ToBoolean(row.Cells["checkBoxColumn"].EditedFormattedValue) == false)
+                    {
+                        isChecked = false;
+                        break;
+                    }
+                }
+                headerCheckBox.Checked = isChecked;
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void viewHelpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void versionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Author:Shiming Xu\r\nDate:2020.4.6\r\nVersion:1.0", "バージョン情報", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            var cnt = 0;
+            if (this.dgv.Rows.Count > 0)
+            {
+                this.tab.TabPages.Clear();
+                foreach (DataGridViewRow row in this.dgv.Rows)
+                {
+                    if (Convert.ToBoolean(row.Cells["checkBoxColumn"].EditedFormattedValue) == false)
+                    {
+                        cnt++;
+                    }
+                    else
+                    {
+                        var client = new RestClient(this.txtEndPoint.Text + "/" + this.txtPath.Text);
+
+                        RestRequest request = new RestRequest();
+                        if (Method.GET.ToString().Equals(this.txtMethod.Text))
+                        {
+                            request = new RestRequest(row.Cells["Request"].Value.ToString(), Method.GET, DataFormat.Json);
+                        }
+                        else if (Method.POST.ToString().Equals(this.txtMethod.Text))
+                        {
+                            request = new RestRequest(row.Cells["Request"].Value.ToString(), Method.POST, DataFormat.Json);
+                        }
+                        else if (Method.PUT.ToString().Equals(this.txtMethod.Text))
+                        {
+                            request = new RestRequest(row.Cells["Request"].Value.ToString(), Method.PUT, DataFormat.Json);
+                        }
+                        if (Method.DELETE.ToString().Equals(this.txtMethod.Text))
+                        {
+                            request = new RestRequest(row.Cells["Request"].Value.ToString(), Method.DELETE, DataFormat.Json);
+                        }
+                        var c = row.Cells["Case"].Value.ToString();
+                        var response = client.Execute(request);
+                    }
+                }
+                if (cnt == this.dgv.Rows.Count)
+                {
+                    MessageBox.Show("実行対象が選択されていません！", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("実行対象が選択されていません！", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (this.dgv.DataSource != null)
+            {
+                if (((DataTable)this.dgv.DataSource).AsEnumerable().Where(x => x["checkBoxColumn"].Equals(true)).Count() > 0)
+                {
+                    ((DataTable)this.dgv.DataSource).AsEnumerable().Where(x => x["checkBoxColumn"].Equals(true)).Select(x => x).ToList().ForEach(y =>
+                    {
+                        var c = y.Field<string>("Case");
+                        var request = y.Field<string>("Request");
+                    });
+                }
+            }
+
+            foreach (DataGridViewRow row in this.dgv.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells["checkBoxColumn"].EditedFormattedValue) == false)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
